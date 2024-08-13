f6a66f3b0cbdaa4fadac4b493ff0dda09e3bffbc
Vanilkaa <gamvanin@gmail.com>
2024-08-13 19:18:15 +0300

    Remove useless parenthesis

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,6 @@
 const leapYears = function(year) {
     return !(year % 100) && !(year & 400) || 
-       (year % 100 && !(year % 4)) ? true : false;
+       year % 100 && !(year % 4) ? true : false;
 };
 
 // Do not edit below this line
