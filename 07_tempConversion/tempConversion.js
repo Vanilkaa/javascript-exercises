76afb38e5a764cd83d657f30700bdb42f40c61f8
Vanilkaa <gamvanin@gmail.com>
2024-08-13 21:11:53 +0300

    Write temp conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    let celsius = (temp - 32) * 5/9;
+    if(celsius % 1) {
+        return +celsius.toFixed(1);
+    }
+    return Math.floor(celsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    let fahrenheit = temp * 9/5 + 32;
+    if(fahrenheit % 1) {
+        return +fahrenheit.toFixed(1);
+    }
+    return Math.floor(fahrenheit);
 };
 
 // Do not edit below this line
