7823810e85867be2764d7ed5c36ce80759ce76aa
Vanilkaa <gamvanin@gmail.com>
2024-08-17 18:55:25 +0300

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let filter = [" ", "!", ",", "."];
+    return string.toLowerCase()
+        .split("") 
+        .filter((char) => !filter.includes(char))
+        .join("") == string.toLowerCase()
+        .split("") 
+        .filter((char) => !filter.includes(char))
+        .reverse().join("");
 };
+
+console.log(palindromes("Race-car! "));
 
 // Do not edit below this line
 module.exports = palindromes;
