e1997ea0eb4be657a92b1a99f86dedea4cdc5230
Vanilkaa <gamvanin@gmail.com>
2024-08-17 19:17:52 +0300

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(+num < 0) return "OOPS";
+    if(+num == 0) return 0;
+    const fib = [1, 1];
+    for(let i = 0; i < +num - 2; i++) {
+        fib.push(fib.at(-1)+fib.at(-2));
+    }
+    return fib.at(-1);
 };
 
 // Do not edit below this line
