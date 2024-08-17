bdb241748c64bd85309360dc67d553a82c8c0af1
Vanilkaa <gamvanin@gmail.com>
2024-08-17 21:18:46 +0300

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.forEach((a) => {
+        if(a.yearOfDeath === undefined) a.yearOfDeath = new Date().getFullYear()
+        });
+    array.sort((a, b) => (a.yearOfDeath-a.yearOfBirth) - (b.yearOfDeath-b.yearOfBirth));
+    return array.at(-1);
 };
 
 // Do not edit below this line
